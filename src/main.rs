97223c07a10cfe12f3118c5d8e10864082e8195f
Chyld Medford <chyld.medford@gmail.com>
2015-05-27 23:47:58 -0700

    fin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::thread;
+
+struct Philosopher {
+    name: String
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string()
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} start eating", self.name);
+        thread::sleep_ms(1000);
+        println!("{} end enting", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("Baruch Spinoza"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Friedrich Nietzsche"),
+        Philosopher::new("Michel Foucault")
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
